c7f417019068f212ab925c45c62201906b03a91c
ies <pgonzalez@ies-sabadell.cat>
2025-05-07 17:07:07 +0200

    Added show-shell-info.sh Bash script

---
+++ act2ej1.sh
@@ -0,0 +1,9 @@
+
+numero = int(input("Introduce un número entero: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
