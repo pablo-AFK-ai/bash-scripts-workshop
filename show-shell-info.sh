7fafeadf789b1441bf81d6eca8965bbe74c8752c
pablo-AFK-ai <pgonzalez@ies-sabadell.cat>
2025-05-08 15:28:04 +0200

    Update show-shell-info.sh

--- show-shell-info.sh
+++ show-shell-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Mostrar las principales variables globales de Bash
+
 echo "Bash version: $BASH_VERSION"
 echo "Home directory: $HOME"
 echo "Current user: $USER"
@@ -9,11 +9,10 @@ echo "Current directory: $PWD"
 echo "Terminal: $TTY"
 echo "Hostname: $HOSTNAME"
 
-# Mostrar todos los argumentos del Shell
+
 echo "Arguments passed to script: $@"
 
-# Mostrar el valor retornado por el último comando ejecutado
 echo "Exit status of last command: $?"
 
-# Mostrar el PID de la Shell actual
+
 echo "PID of the current shell: $$"
